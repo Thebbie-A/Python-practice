1d4ddc01567c8a49ec5df2a81bebbc242655de06
Thebbie-A <thebbie.arthur@gmail.com>
2020-05-23 23:15:56 +0000

    add second practice

---
+++ area_of_a_field/area_of_a_field.py
@@ -0,0 +1,7 @@
+
+square_feet =43560
+length = float(input( 'Enter the length of the field in feet ; ' ))
+width = float(input( 'Enter the length of the field in feet ; ' ))
+area = length * width
+arces = length * width / square_feet
+print(arces)
