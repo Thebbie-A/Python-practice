ee470532bae581d9e77eed13d685d69473398b16
Thebbie-A <thebbie.arthur@gmail.com>
2020-06-04 01:03:01 +0000

    adding a folder

---
+++ area_of_a_triangle/area_of_a_triangle.py
@@ -0,0 +1,10 @@
+
+# b = length , h= height
+# area = b * h / 2
+
+value_b = input('Enter value for b ')
+value_h = input('Enter value for h ')
+
+# calculating area of triangle
+area_of_triangle = 5 * 8 /2
+print(area_of_triangle)
