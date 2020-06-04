9c91081b8a02f14432e92fcc270231c02f9705e5
Thebbie-A <thebbie.arthur@gmail.com>
2020-06-04 01:12:38 +0000

    added practice

---
+++ area_and_volume/area_and_volume.py
@@ -0,0 +1,9 @@
+#displaying radius, assuming raduis = 5
+radius = 5
+ # area of a circle = pi(3.14) , r-squared
+area_of_a_cirlce = 3.14 * 5**2
+print(area_of_a_cirlce)
+
+#   calculating the volume of a sphere
+volume_of_sphere = 1.333 * 3.14 * 5 ** 2
+print(volume_of_sphere)
