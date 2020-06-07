1152e4c5b6547a0a425c30801e57b59341aa3c80
Thebbie-A <thebbie.arthur@gmail.com>
2020-06-07 20:49:12 +0000

    add practice

---
+++ area_of_a_regular_polygon/area_of_a_regular_polygon.py
@@ -0,0 +1,12 @@
+
+# input values
+length_of_sides = input( 'Enter the length of sides of the polygon ')
+no_of_sides = input('Enter the number of sides of the polygon ')
+
+# Finding the area of polygon
+area = int(length_of_sides) * int(no_of_sides)
+area_1 = 256
+area_2 = 0.055
+total_area = int(area_1) / int(area_2)
+print(total_area)
+
