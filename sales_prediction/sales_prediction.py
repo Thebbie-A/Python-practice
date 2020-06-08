190bde7257babb07a465cc7d1688ba0f06507d59
Thebbie-A <thebbie.arthur@gmail.com>
2020-06-08 00:22:09 +0000

    add practice

---
+++ sales_prediction/sales_prediction.py
@@ -0,0 +1,10 @@
+
+# annual profit of total sales
+annual_profit = 0.23
+
+# display projected amount
+projected_amounts = input('Enter projected amount of total sales ')
+
+# total_sales = int(annual_profit) * int(projected_amounts)
+total_sales = 0.23 * 50
+print(total_sales)
