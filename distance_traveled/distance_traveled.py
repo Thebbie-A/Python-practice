4bfb472bfbff571d6fa0805a38919cde34da41ca
Thebbie-A <thebbie.arthur@gmail.com>
2020-06-20 06:00:03 +0000

    added practice

---
+++ distance_traveled/distance_traveled.py
@@ -0,0 +1,17 @@
+# Assuming there are no accidents or delays, the distance that a car travels down the interstate can be
+# calculated with the following formula:
+# Distance = Speed * Time
+# A car is traveling at 70 miles per hour. Write a program that displays the following:
+# • The distance the car will travel in 6 hours
+# • The distance the car will travel in 10 hours
+# • The distance the car will travel in 15 hours
+
+speed = 70
+distance_in_6hrs = 70 * 6
+distance_in_10hrs = 70 * 10
+distance_in_15hrs = 70 * 15
+
+print(distance_in_6hrs)
+print(distance_in_10hrs)
+print(distance_in_15hrs)
+
