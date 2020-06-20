f50875cc9f93b50c9a181371cd0048a5a2f4bdda
Thebbie-A <thebbie.arthur@gmail.com>
2020-06-20 05:57:38 +0000

    added practice

---
+++ algorithm_workbench/algorithm_workbench.py
@@ -0,0 +1,41 @@
+
+# Write Python code that prompts the user to enter his or her height and assigns the user’s
+# input to a variable named height
+
+your_height = int(input('Enter your height '))
+height = your_height
+print(height)
+
+#
+# # . Write Python code that prompts the user to enter his or her favorite color and assigns
+# # the user’s input to a variable named color.
+#
+favorite_color = input( 'Enter your favorite color ')
+color = favorite_color
+print(color)
+
+# Write a Python statement that assigns the sum of 10 and 14 to the variable total.
+var1 = 4
+var2 = 6
+var3 = int(var1 + var2)
+
+sum1 = 10
+sum2 = 4
+sum3 = int(sum1 + sum2)
+total = int(var3 + sum3)
+print(var3 + sum3)
+
+# Write a Python statement that subtracts the variable down_payment from the variable
+# total and assigns the result to the variable due
+
+down_payment = 15
+due = int(total - down_payment)
+print(due)
+
+
+# Write a Python statement that multiplies the variable subtotal by 0.15 and assigns the
+# result to the variable total.
+
+subtotal = 8
+# number = 0.15
+print(subtotal * 0.15)
