226d92acb5afc2114eeac5555f55cb26ade863a1
Deborah Arthur <thebbie.arthur@gmail.com>
2022-03-23 03:01:33 +0000

    Add files via upload

---
+++ twikle little_star.py
@@ -0,0 +1,5 @@
+print('Twinkle, twinkle, little star,\n \t '
+	  'How I wonder what you are! \n\t Up above the world so high,Up above the world so high,'
+	  '\n\t Like a diamond in the sky. Twinkle, twinkle, little star,')
+
+#print('How I wonder what you are.')
